e5b1c676cf6dab3035389b408a8745b95696b744
Cyber_TECHF <hdjdjdjxjndj@gmail.com>
2024-08-21 10:53:44 +0100

    Site Web Location_Voiture
    Les languages utiliser : HTML , CSS , JAVASCRIPT

---
+++ Location_voiture/backend/script.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const prix = document.getElementById("prix");
+    prix.addEventListener("mouseover",function(){
+        const price = document.getElementById("price");
+        price.style.display = "block";
+    })
+
+    prix.addEventListener("mouseout",function(){
+        const price = document.getElementById("price");
+        price.style.display = "none";
+    })
+
+    const prix1 = document.getElementById("prix1");
+    prix1.addEventListener("mouseover", function(){
+        price1.style.display = "block";
+    })
+
+    prix1.addEventListener("mouseout",function(){
+        price1.style.display = "none";
+    })
+
+    const prix2 = document.getElementById("prix2");
+    prix2.addEventListener("mouseover", function(){
+        const price2 = document.getElementById("price2");
+        price2.style.display = "block";
+
+    })
+
+    prix2.addEventListener("mouseout", function(){
+        const price2 = document.getElementById("price2");
+        price2.style.display = "none";
+
+    })
+
+    const prix3 = document.getElementById("prix3");
+    prix3.addEventListener("mouseover", function(){
+        const price3 = document.getElementById("price3");
+        price3.style.display = "block";
+
+    })
+
+    prix3.addEventListener("mouseout", function(){
+        const price3 = document.getElementById("price3");
+        price3.style.display = "none";
+
+    })
+
+
+    const prix4 = document.getElementById("prix4");
+    prix4.addEventListener("mouseover", function(){
+        const price4 = document.getElementById("price4");
+        price4.style.display = "block";
+
+    })
+
+    prix4.addEventListener("mouseout", function(){
+        const price4 = document.getElementById("price4");
+        price4.style.display = "none";
+
+    })
+
+
+    const prix5 = document.getElementById("prix5");
+    prix5.addEventListener("mouseover", function(){
+        const price5 = document.getElementById("price5");
+        price5.style.display = "block";
+
+    })
+
+    prix5.addEventListener("mouseout", function(){
+        const price5 = document.getElementById("price5");
+        price5.style.display = "none";
+
+    })
+})
